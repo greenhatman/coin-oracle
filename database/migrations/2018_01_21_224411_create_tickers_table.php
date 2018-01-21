45808dda488d9ae96b3b9d084813b18ecd5d6bfd
Albert Cloete <albert@yalidev.co.za>
2018-01-21 23:05:44 +0000

    Added migrations for exchanges and tickers.

---
+++ database/migrations/2018_01_21_224411_create_tickers_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateTickersTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('tickers', function (Blueprint $table) {
+            $table->increments('id');
+            $table->integer('exchange_id')->unsigned();
+            $table->foreign('exchange_id')->references('id')->on('exchanges')
+                ->onDelete('restrict');
+            $table->decimal('price', 13, 4)->nullable();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('tickers');
+    }
+}
